3378abe4d09fbb8b74b4ffc237aaa66ae1c8ba42
SergeiSuvorov <78319712+SergeiSuvorov@users.noreply.github.com>
2022-07-01 00:19:12 +0300

    HomeWork3

--- Assets/Scripts/PlayFabLogin.cs
+++ Assets/Scripts/PlayFabLogin.cs
@@ -1,9 +1,26 @@
 ﻿using PlayFab;
 using PlayFab.ClientModels;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
 public class PlayFabLogin : MonoBehaviour
 {
-    public void Start()
+    [SerializeField] Button _connectButton;
+    [SerializeField] TMP_Text _textResult;
+
+   
+    private void Start()
+    {
+        _connectButton.onClick.AddListener(Connect);
+    }
+
+    private void OnDestroy()
+    {
+        _connectButton.onClick.RemoveAllListeners();
+    }
+
+    private void Connect()
     {
         // Here we need to check whether TitleId property is configured in settings or not
 
@@ -13,22 +30,27 @@ public class PlayFabLogin : MonoBehaviour
             * If not we need to assign it to the appropriate variable manually
             * Otherwise we can just remove this if statement at all
             */
-            PlayFabSettings.staticSettings.TitleId = " A823B";
+            PlayFabSettings.staticSettings.TitleId = " DFCB9";
         }
         var request = new LoginWithCustomIDRequest
         {
-            CustomId = "GeekBrainsLesson3",
+            CustomId = "Player1",
             CreateAccount = true
         };
         PlayFabClientAPI.LoginWithCustomID(request, OnLoginSuccess, OnLoginFailure);
     }
+
     private void OnLoginSuccess(LoginResult result)
     {
         Debug.Log("Congratulations, you made successful API call!");
+        _textResult.text = "Connect";
+        _textResult.color = Color.green;
     }
     private void OnLoginFailure(PlayFabError error)
     {
         var errorMessage = error.GenerateErrorReport();
         Debug.LogError($"Something went wrong: {errorMessage}");
+        _textResult.text = "Error";
+        _textResult.color = Color.red;
     }
 }
